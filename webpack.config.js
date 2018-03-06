23db64ddf1be10062ab17729d9fd931437f588fd
Sil <sil@matise.nl>
2018-03-06 21:59:05 +0100

    cleaned up config and added functions

--- webpack.config.js
+++ webpack.config.js
@@ -14,33 +14,45 @@ module.exports = {
 		filename: 'main.js',
 		path: `${rootPath}/dist`
 	},
-	module: {
-		rules: [
-			/*
-			your other rules for JavaScript transpiling go in here
-			*/
-			// { // regular css files
-			// 	test: /\.css$/,
-			// 	loader: ExtractTextPlugin.extract({
-			// 		loader: 'css-loader?importLoaders=1',
-			// 	}),
-			// },
-			{ // sass / scss loader for webpack
-				test: /\.(sass|scss)$/,
-				loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-			}
-		]
+	plugins: {
+		'autoprefixer': {}
 	},
-	plugins: [
-		new CopyWebpackPlugin([{
-			from: 'src/scss/',
-			to: 'scss/'
-		}]),
-		new ExtractTextPlugin({ // define where to save the file
-			filename: 'style.css',
-			allChunks: true,
-		}),
-	],
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].css',
+						outputPath: 'dist/css/'
+					}
+				},
+				{
+					loader: 'extract-loader'
+				},
+				{
+					loader: 'css-loader'
+				},
+				{
+					loader: 'postcss-loader'
+				},
+				{
+					loader: 'sass-loader'
+				}
+			]
+		}]
+	},
+	// plugins: [
+	// 	new CopyWebpackPlugin([{
+	// 		from: 'src/scss/',
+	// 		to: 'scss/'
+	// 	}]),
+	// 	new ExtractTextPlugin({ // define where to save the file
+	// 		filename: 'style.css',
+	// 		allChunks: true,
+	// 	}),
+	// ],
+	watch: true,
 	watchOptions: {
 		aggregateTimeout: 300,
 		poll: 1000
