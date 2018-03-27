563e6ac647a13203828d9269f8ad566f50776ca3
Sil <sil@matise.nl>
2018-03-27 21:06:06 +0200

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,61 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
-    filename: "style.css"
+	filename: 'style.css'
 });
 
 const extractHtml = new ExtractTextPlugin({
-    filename: "index.html"
+	filename: 'index.html'
 });
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
-            },
-            {
-                test: /\.pug$/,
-                use: extractHtml.extract({
-                    use: [{
-                        loader: "raw-loader"
-                    },{
-                        loader: "pug-html-loader"
-                    }],
-                })
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            }, {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        extractSass,
-        extractHtml
-    ]
-}
+	entry: './index.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'main.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: extractSass.extract({
+					use: [
+						{
+							loader: 'css-loader'
+						},
+						{
+							loader: 'sass-loader'
+						}
+					],
+					// use style-loader in development
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.pug$/,
+				use: extractHtml.extract({
+					use: [
+						{
+							loader: 'raw-loader'
+						},
+						{
+							loader: 'pug-html-loader'
+						}
+					]
+				})
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	plugins: [extractSass, extractHtml]
+};
