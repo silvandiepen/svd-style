a165bc259549663434c9d2788f02fd7c081b0ab3
Bassel Al Araaj <info@basselalaraaj.com>
2018-03-27 20:30:48 +0200

    exporting stylesheet and js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import styleSheet from './index.scss';
 console.log('Welcome');
 // require('html-loader?name=[name].[ext]!./static/index.pug');
 // require('sass-loader?name=[name].[ext]!./src/scss/style.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,42 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractCSS = new ExtractTextPlugin({ filename: 'dist/style.css' })
+const extractSass = new ExtractTextPlugin({
+    filename: "style.css"
+});
 
 module.exports = {
-	entry: './index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js'
-	},
-	module: {
-		rules: [
-			{ test: /\.pug/, use: 'pug-html-loader' },
-			{
-				test: /\.scss$/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'sass-loader' },
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		extractCSS
-  ]
-};
+    entry: './index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }, {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        extractSass
+    ]
+}
