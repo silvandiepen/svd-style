f964daa710ae257e0ac293b2e066d3e010f6fa61
Sil <sil@matise.nl>
2018-05-23 09:34:57 +0200

    debugging my ass off..

--- scripts/css-functions.js
+++ scripts/css-functions.js
@@ -1,4 +1,4 @@
-let convert = require('color-convert');
+let convert = require("color-convert");
 
 let functions = {};
 
@@ -12,21 +12,21 @@ functions.isNumber = n => {
 functions.grid = function(data, v) {
 	function gridCalc(g) {
 		let grid = 100 / data.grid.columns;
-		return grid * g + 'vw';
+		return grid * g + "vw";
 	}
 	if (functions.isNumber(v)) {
 		return gridCalc(v);
 	} else {
-		if (v.split(' ').length > 1) {
+		if (v.split(" ").length > 1) {
 			let multi = [];
-			v.split(' ').forEach(function(val) {
+			v.split(" ").forEach(function(val) {
 				if (functions.isNumber(val)) {
 					multi.push(gridCalc(val));
 				} else {
-					console.log(val, ' - Nan');
+					console.log(val, " - Nan");
 				}
 			});
-			return multi.join(' ');
+			return multi.join(" ");
 		} else {
 			return v;
 		}
@@ -41,28 +41,30 @@ functions.color = (data, v) => {
 	let searchColor = v;
 	let transparency = 1;
 
-	let vArray = v.split(',');
+	let vArray = v.split(",");
 	if (vArray.length > 1) {
 		searchColor = vArray[0];
 		transparency = vArray[1];
 	}
-	//console.log(data.colors);
+	// console.log(data.colors);
 	Object.keys(data.colors).forEach(function(list) {
-		data.colors[list].forEach(function(colorList) {
-			Object.keys(colorList).map(function(value, key) {
-				if (value.toLowerCase() === searchColor.toLowerCase()) {
-					theColor = colorList[value];
-					if (transparency < 1) {
-						theColor =
-							'rgba(' +
-							convert.hex.rgb(theColor.substring(1)).join(',') +
-							',' +
-							transparency +
-							')';
+		if (list !== "title") {
+			data.colors[list].forEach(function(colorList) {
+				Object.keys(colorList).map(function(value, key) {
+					if (value.toLowerCase() === searchColor.toLowerCase()) {
+						theColor = colorList[value];
+						if (transparency < 1) {
+							theColor =
+								"rgba(" +
+								convert.hex.rgb(theColor.substring(1)).join(",") +
+								"," +
+								transparency +
+								")";
+						}
 					}
-				}
+				});
 			});
-		});
+		}
 	});
 	return theColor;
 };
@@ -72,7 +74,7 @@ functions.color = (data, v) => {
 */
 functions.assets = (data, v) => {
 	let theValue = null;
-	let vArray = v.split('.');
+	let vArray = v.split(".");
 	if (vArray.length > 1) {
 		if (vArray.length == 3) {
 			if (data[vArray[0]][vArray[1]][vArray[2]]) {
