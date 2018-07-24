4ad099d6af81932d2f53b5f0eb82eebefb1c71c6
Sil <sil@matise.nl>
2018-07-24 10:21:17 +0200

    full rework with outputs and improvs

--- index.js
+++
@@ -1,1 +0,0 @@
-import styleSheet from "./index.scss";
--- scripts/json-to-css.js
+++ scripts/json-to-css.js
@@ -1,18 +1,18 @@
-let fs = require('fs'),
-	path = require('path'),
-	flatten = require('flat'),
-	functions = require('./css-functions.js');
+let fs = require("fs"),
+	path = require("path"),
+	flatten = require("flat"),
+	functions = require("./css-functions.js");
 
-let delimiter = '-',
-	sourceFolder = 'src/settings',
-	distFolder = 'src/scss/';
+let delimiter = "-",
+	sourceFolder = "data/",
+	distFolder = "src/scss/";
 
 let fileTypes = [
 	{
-		type: 'scss',
-		dest: distFolder + 'settings',
-		varPattern: '${{var}}: {{value}} !default; //',
-		listPatternParent: '${{var}}: (\n{{list}}\n) !default; //',
+		type: "scss",
+		dest: distFolder + "settings",
+		varPattern: "${{var}}: {{value}} !default; //",
+		listPatternParent: "${{var}}: (\n{{list}}\n) !default; //",
 		listSubPatternParent: '\t"{{var}}": (\n{{list}}\n\t) //',
 		listPattern: '\t"{{var}}": {{value}}'
 	}
@@ -38,9 +38,9 @@ function getFiles(dir, files_) {
 	files_ = files_ || [];
 	var files = fs.readdirSync(dir);
 	for (var i in files) {
-		if (files[i].split('.')[files[i].split('.').length - 1] === 'json') {
+		if (files[i].split(".")[files[i].split(".").length - 1] === "json") {
 			//	console.log(files[i]);
-			var name = dir + '/' + files[i];
+			var name = dir + "/" + files[i];
 			if (fs.statSync(name).isDirectory()) {
 				getFiles(name, files_);
 			} else {
@@ -56,49 +56,49 @@ function getFiles(dir, files_) {
 let stored = {};
 getFiles(sourceFolder).forEach(file => {
 	//	let fileName = file.split('/')[file.split('/').length - 1].replace('.json', '');
-	Object.assign(stored, JSON.parse(fs.readFileSync(file, 'utf8')));
+	Object.assign(stored, JSON.parse(fs.readFileSync(file, "utf8")));
 });
 
 function removeKeys(value) {
-	return value.replace('-0-', '-');
+	return value.replace("-0-", "-");
 }
 
 // Do functions when necessary;
 function doFunction(value) {
-	let thefunc = value.replace('{{', '').replace('}}', '');
-	let func = thefunc.split('(')[0];
-	let parameters = value.replace(/(^.*\(|\).*$)/g, '');
+	let thefunc = value.replace("{{", "").replace("}}", "");
+	let func = thefunc.split("(")[0];
+	let parameters = value.replace(/(^.*\(|\).*$)/g, "");
 	let newvalue;
-	if (typeof functions[func] === 'function') {
+	if (typeof functions[func] === "function") {
 		newvalue = functions[func](stored, parameters);
 	} else {
-		newvalue = func + '(' + parameters + ')';
+		newvalue = func + "(" + parameters + ")";
 	}
 	return newvalue;
 }
 
 // Do functions when necessary;
 function makeList(value) {
-	let newvalue = value.replace('[[', '').replace(']]', '');
+	let newvalue = value.replace("[[", "").replace("]]", "");
 	return newvalue;
 }
 
 function stringValue(value) {
-	if (typeof value == 'string') {
+	if (typeof value == "string") {
 		let quotes = true;
-		let noquotes = ['normal', 'regular', 'lowercase', 'uppercase', 'none', '0'];
+		let noquotes = ["normal", "regular", "lowercase", "uppercase", "none", "0"];
 		if (
-			value.indexOf('px') > -1 ||
-			value.indexOf('ch') > -1 ||
-			value.indexOf('rem') > -1 ||
-			value.indexOf('%') > -1
+			value.indexOf("px") > -1 ||
+			value.indexOf("ch") > -1 ||
+			value.indexOf("rem") > -1 ||
+			value.indexOf("%") > -1
 		) {
 			quotes = false;
 		}
-		if (value.indexOf('(') > -1 && value.indexOf(')') > -1) {
+		if (value.indexOf("(") > -1 && value.indexOf(")") > -1) {
 			quotes = false;
 		}
-		if (value.charAt(0) === '#') {
+		if (value.charAt(0) === "#") {
 			quotes = false;
 		}
 		if (noquotes.includes(value)) {
@@ -108,12 +108,12 @@ function stringValue(value) {
 			quotes = false;
 		}
 		// Do Functions
-		if (value.indexOf('{{') > -1) {
+		if (value.indexOf("{{") > -1) {
 			value = stringValue(doFunction(value));
 		}
 		// makeList
 		if (value) {
-			if (value.indexOf('[[') > -1) {
+			if (value.indexOf("[[") > -1) {
 				value = makeList(value);
 			}
 		}
@@ -141,10 +141,10 @@ function objToStyle(file, type) {
 
 	// Do the variables
 	Object.keys(data).forEach(key => {
-		if (!functions.isNumber(key.split('-')[key.split('-').length - 1])) {
+		if (!functions.isNumber(key.split("-")[key.split("-").length - 1])) {
 			variable = type.varPattern
-				.replace('{{var}}', removeKeys(key.toLowerCase()))
-				.replace('{{value}}', stringValue(data[key]));
+				.replace("{{var}}", removeKeys(key.toLowerCase()))
+				.replace("{{value}}", stringValue(data[key]));
 			newFile.push(removeKeys(variable));
 		}
 	});
@@ -154,48 +154,48 @@ function objToStyle(file, type) {
 		let list = [];
 
 		Object.keys(object).forEach(key => {
-			if (typeof object[key][0] === 'object') {
+			if (typeof object[key][0] === "object") {
 				if (iteration > 0) {
 					list.push(
 						type.listSubPatternParent
-							.replace('{{var}}', removeKeys(key.toLowerCase()))
+							.replace("{{var}}", removeKeys(key.toLowerCase()))
 							.replace(
-								'{{list}}',
+								"{{list}}",
 								stringify(object[key][0], iteration + 1, true)
 							)
 					);
 				} else {
 					list.push(
 						type.listPatternParent
-							.replace('{{var}}', removeKeys(key.toLowerCase()))
+							.replace("{{var}}", removeKeys(key.toLowerCase()))
 							.replace(
-								'{{list}}',
+								"{{list}}",
 								stringify(object[key][0], iteration + 1, true)
 							)
 					);
 				}
 			} else if (
-				Object.prototype.toString.call(object[key]) === '[object Array]'
+				Object.prototype.toString.call(object[key]) === "[object Array]"
 			) {
 				list.push(
 					type.listPatternParent
-						.replace('{{var}}', removeKeys(key.toLowerCase()))
-						.replace('{{list}}', '"' + object[key].join('", \r\n"') + '"')
+						.replace("{{var}}", removeKeys(key.toLowerCase()))
+						.replace("{{list}}", '"' + object[key].join('", \r\n"') + '"')
 				);
 			} else if (inObjectList) {
 				list.push(
 					type.listPattern
-						.replace('{{var}}', key)
-						.replace('{{value}}', stringValue(object[key]))
+						.replace("{{var}}", key)
+						.replace("{{value}}", stringValue(object[key]))
 				);
 			}
 		});
-		return list.join(', \r\n');
+		return list.join(", \r\n");
 	}
 
 	newFile.push(stringify(listData, 0));
 
-	return newFile.join('\r\n');
+	return newFile.join("\r\n");
 }
 
 /**
@@ -204,7 +204,7 @@ function objToStyle(file, type) {
 function makeDirs() {
 	fileTypes.forEach(type => {
 		const sep = path.sep;
-		const initDir = path.isAbsolute(type.dest) ? sep : '';
+		const initDir = path.isAbsolute(type.dest) ? sep : "";
 		type.dest.split(sep).reduce((parentDir, childDir) => {
 			const curDir = path.resolve(parentDir, childDir);
 			if (!fs.existsSync(curDir)) {
@@ -217,15 +217,15 @@ function makeDirs() {
 makeDirs();
 
 function createImport(files) {
-	let code = '';
+	let code = "";
 	files.forEach(function(file) {
 		code = code + '@import "' + file + '";\n';
 	});
 	console.log(code);
 
 	fileTypes.forEach(type => {
-		fs.writeFileSync(type.dest + '/_index.' + type.type, code, function(err) {
-			console.log('woops, something went wrong!' + err);
+		fs.writeFileSync(type.dest + "/_index." + type.type, code, function(err) {
+			console.log("woops, something went wrong!" + err);
 		});
 	});
 }
@@ -240,38 +240,38 @@ getFiles(sourceFolder).forEach(file => {
 	i++;
 
 	let fileName = file
-		.split('/')
-		[file.split('/').length - 1].replace('.json', '');
-	console.log('\x1b[33m%s\x1b[0m', fileName);
-	if (fileName.charAt(0) !== '_') {
+		.split("/")
+		[file.split("/").length - 1].replace(".json", "");
+	console.log("\x1b[33m%s\x1b[0m", fileName);
+	if (fileName.charAt(0) !== "_") {
 		fileTypes.forEach(type => {
 			// Write New Files
 			let compiled = objToStyle(
-				JSON.parse(fs.readFileSync(file, 'utf8')),
+				JSON.parse(fs.readFileSync(file, "utf8")),
 				type
 			);
 
 			// Fix for less lists;
-			compiled = compiled.replaceAll(';,', ';');
+			compiled = compiled.replaceAll(";,", ";");
 
-			console.log('\x1b[32m%s\x1b[0m', '\t\u2713', type.type);
+			console.log("\x1b[32m%s\x1b[0m", "\t\u2713", type.type);
 			console.log(
-				'\x1b[32m%s\x1b[0m',
-				'\t  ' +
+				"\x1b[32m%s\x1b[0m",
+				"\t  " +
 					file +
-					' \u2192 ' +
+					" \u2192 " +
 					type.dest +
-					'/_' +
+					"/_" +
 					fileName +
-					'.' +
+					"." +
 					type.type
 			);
 
 			fs.writeFileSync(
-				type.dest + '/_' + fileName + '.' + type.type,
+				type.dest + "/_" + fileName + "." + type.type,
 				compiled,
 				function(err) {
-					console.log('woops, something went wrong!' + err);
+					console.log("woops, something went wrong!" + err);
 				}
 			);
 		});
